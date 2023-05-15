1c3fcedbc1062e053709454688db8b04bbc3f9ac
Aleksandr <96977037+Balaberd@users.noreply.github.com>
2023-05-15 13:15:21 +0500

    + ganre, tag filter

--- src/shared/index.ts
+++ src/shared/index.ts
@@ -1,2 +1,4 @@
 export { Poster } from "./ui/Poster/Poster";
 export { RatingLine } from "./ui/RatingLine/RatingLine";
+export { useResize } from "./hooks/resizeHook";
+export { Trailer } from "./ui/Trailer/Trailer";
