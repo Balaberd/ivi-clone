2e229947f3fc9fb378f46c72054649382b7d34b5
Aleksandr <96977037+Balaberd@users.noreply.github.com>
2023-05-15 15:20:07 +0500

    Рефакторинг

--- src/widgets/ui/Footer/Footer.tsx
+++ src/widgets/ui/Footer/Footer.tsx
@@ -6,93 +6,129 @@ import DeviceLink from "./ui/DeviceLink";
 
 export const Footer: FC = () => (
   <footer className={styles.container}>
-    <div className={styles.wrapper}>
+    <div className={cn(styles.line, styles.line_first)}>
       <div className={cn(styles.column, styles.aboutUs)}>
-        <span className={styles.columnTitle}>О нас</span>
+        <span className={styles.title}>О нас</span>
         <ul>
           <li>
-            <a href="https://corp.ivi.ru/?_gl=1%2A9ld3wl%2A_ga%2AOTE5MTg2MTQ3LjE2ODE1NTMxMTk.%2A_ga_GETQ4387MJ%2AMTY4MzYxNzQxMi4xNi4wLjE2ODM2MTc0MTIuNjAuMC4w">
+            <a
+              className={styles.link}
+              href="https://corp.ivi.ru/?_gl=1%2A9ld3wl%2A_ga%2AOTE5MTg2MTQ3LjE2ODE1NTMxMTk.%2A_ga_GETQ4387MJ%2AMTY4MzYxNzQxMi4xNi4wLjE2ODM2MTc0MTIuNjAuMC4w"
+            >
               О компании
             </a>
           </li>
           <li>
-            <a href="https://corp.ivi.ru/career/?_gl=1*jlt6u7*_ga*OTE5MTg2MTQ3LjE2ODE1NTMxMTk.*_ga_GETQ4387MJ*MTY4MzYxNzQxMi4xNi4wLjE2ODM2MTc0MTIuNjAuMC4w#career-vacancy-block">
+            <a
+              className={styles.link}
+              href="https://corp.ivi.ru/career/?_gl=1*jlt6u7*_ga*OTE5MTg2MTQ3LjE2ODE1NTMxMTk.*_ga_GETQ4387MJ*MTY4MzYxNzQxMi4xNi4wLjE2ODM2MTc0MTIuNjAuMC4w#career-vacancy-block"
+            >
               Вакансии
             </a>
           </li>
           <li>
-            <a href="https://www.ivi.ru/pages/beta/">
+            <a className={styles.link} href="https://www.ivi.ru/pages/beta/">
               Программа бета-тестирования
             </a>
           </li>
           <li>
-            <a href="https://www.ivi.ru/info/partners">
+            <a className={styles.link} href="https://www.ivi.ru/info/partners">
               Информация для партнёров
             </a>
           </li>
           <li>
-            <a href="https://corp.ivi.ru/advertisers/?_gl=1*22we9j*_ga*OTE5MTg2MTQ3LjE2ODE1NTMxMTk.*_ga_GETQ4387MJ*MTY4MzYxNzQxMi4xNi4xLjE2ODM2MTk0MzkuNDMuMC4w">
+            <a
+              className={styles.link}
+              href="https://corp.ivi.ru/advertisers/?_gl=1*22we9j*_ga*OTE5MTg2MTQ3LjE2ODE1NTMxMTk.*_ga_GETQ4387MJ*MTY4MzYxNzQxMi4xNi4xLjE2ODM2MTk0MzkuNDMuMC4w"
+            >
               Размещение рекламы
             </a>
           </li>
           <li>
-            <a href="https://www.ivi.ru/info/agreement">
+            <a className={styles.link} href="https://www.ivi.ru/info/agreement">
               Пользовательское соглашение
             </a>
           </li>
           <li>
-            <a href="https://www.ivi.ru/info/confidential">
+            <a
+              className={styles.link}
+              href="https://www.ivi.ru/info/confidential"
+            >
               Политика конфиденциальности
             </a>
           </li>
           <li>
-            <a href="https://www.ivi.ru/info/goryachaya-liniya-komplaens">
+            <a
+              className={styles.link}
+              href="https://www.ivi.ru/info/goryachaya-liniya-komplaens"
+            >
               Комплаенс
             </a>
           </li>
         </ul>
       </div>
       <div className={cn(styles.column, styles.sections)}>
-        <span className={styles.columnTitle}>Разделы</span>
+        <span className={styles.title}>Разделы</span>
         <ul>
           <li>
-            <a href="https://www.ivi.ru/">Мой Иви</a>
+            <a className={styles.link} href="https://www.ivi.ru/">
+              Мой Иви
+            </a>
           </li>
           <li>
-            <a href="https://www.ivi.ru/new">Что нового</a>
+            <a className={styles.link} href="https://www.ivi.ru/new">
+              Что нового
+            </a>
           </li>
           <li>
-            <a href="https://www.ivi.ru/movies">Фильмы</a>
+            <a className={styles.link} href="https://www.ivi.ru/movies">
+              Фильмы
+            </a>
           </li>
           <li>
-            <a href="https://www.ivi.ru/series">Сериалы</a>
+            <a className={styles.link} href="https://www.ivi.ru/series">
+              Сериалы
+            </a>
           </li>
           <li>
-            <a href="https://www.ivi.ru/animation">Мультфильмы</a>
+            <a className={styles.link} href="https://www.ivi.ru/animation">
+              Мультфильмы
+            </a>
           </li>
           <li>
-            <a href="https://www.ivi.ru/tvplus">TV+</a>
+            <a className={styles.link} href="https://www.ivi.ru/tvplus">
+              TV+
+            </a>
           </li>
           <li>
-            <a href="https://www.ivi.ru/goodmovies">Что посмотреть</a>
+            <a className={styles.link} href="https://www.ivi.ru/goodmovies">
+              Что посмотреть
+            </a>
           </li>
           <li>
-            <a href="https://www.ivi.ru/cert" className={styles.activate}>
+            <a
+              className={cn(styles.link, styles.activate)}
+              href="https://www.ivi.ru/cert"
+            >
               Активация сертификата
             </a>
           </li>
         </ul>
       </div>
-      <div className={styles.supportColumn}>
-        <span className={styles.columnTitle}>Служба поддержки</span>
+      <div className={cn(styles.column, styles.supportColumn)}>
+        <span className={styles.title}>Служба поддержки</span>
         <p>
-          Мы всегда готовы вам помочь. <br />
+          Мы всегда готовы вам помочь.
+          <br />
           Наши операторы онлайн 24/7
         </p>
-        <a href="https://www.ivi.ru/profile" className={styles.textInChat}>
+        <a
+          href="https://www.ivi.ru/profile"
+          className={cn(styles.button, styles.button_textInChat)}
+        >
           Написать в чате
         </a>
-        <div className={styles.supportIcons}>
+        <div className={styles.supportContactsBlock}>
           <a href="mailto:support@ivi.ru" className={styles.mailCall}>
             <span className={styles.mail}></span>
           </a>
@@ -109,12 +145,14 @@ export const Footer: FC = () => (
         <p>Ответы на вопросы</p>
       </div>
       <div className={cn(styles.column, styles.watch)}>
-        <span className={styles.megaphone}></span>
-        <p>Смотрите фильмы, сериалы и мультфильмы без рекламы</p>
+        <div>
+          <span className={styles.megaphone}></span>
+          <p>Смотрите фильмы, сериалы и мультфильмы без рекламы</p>
+        </div>
       </div>
     </div>
 
-    <div className={styles.devicesNSocials}>
+    <div className={cn(styles.line, styles.line_second)}>
       <div className={styles.devicesBlock}>
         <DeviceLink
           link="https://apps.apple.com/RU/app/id455705533?mt=8"
@@ -173,6 +211,7 @@ export const Footer: FC = () => (
         />
       </div>
     </div>
+
     <div className={styles.copyright}>
       <p>© 2023 ООО «Иви.ру»</p>
       <p>
