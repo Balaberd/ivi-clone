ae31007a14742092f90f4ab39571fda865215c73
Aleksandr <96977037+Balaberd@users.noreply.github.com>
2023-05-15 15:33:58 +0500

    Рефакторинг

--- src/entities/index.ts
+++ src/entities/index.ts
@@ -1,2 +1,5 @@
 export { MovieCard } from "./movie/ui/MovieCard/MovieCard";
 export { BannerCard } from "./movie/ui/BannerCard/BannerCard";
+export { GenreCard } from "./filter/ui/GenreCard";
+export { TopMovieCard } from "./movie/ui/TopMovieCard/TopMovieCard";
+export { PersonCard } from "./person/PersonCard/PersonCard";
--- src/widgets/index.ts
+++ src/widgets/index.ts
@@ -1,8 +1,9 @@
-export { TopMovieSlider } from "./ui/TopMovieSlider/TopMovieSlider";
 export { BannerSlider } from "./ui/BannerSlider/BannerSlider";
-export { MovieSlider } from "./ui/MovieSlider/MovieSlider";
-export { Header } from "./ui/Header/Header";
-export { HeaderMobile } from "./ui/Header/HeaderMobile/HeaderMobile";
-export { MainFilter } from "./ui/MainFilter/MainFilter";
+export { FilterByGenre } from "./ui/FilterByGenre/FilterByGenre";
 export { FilterByTag } from "./ui/FilterByTag/FilterByTag";
 export { Footer } from "./ui/Footer/Footer";
+export { Header } from "./ui/Header/Header";
+export { MainFilter } from "./ui/MainFilter/MainFilter";
+export { MovieSlider } from "./ui/MovieSlider/MovieSlider";
+export { PersonSlider } from "./ui/PersonSlider/PersonSlider";
+export { TopMovieSlider } from "./ui/TopMovieSlider/TopMovieSlider";
